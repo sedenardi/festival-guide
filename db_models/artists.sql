848a0ff36252a1586876d49a5748afb2030a67a7
Sanders DeNardi <sedenardi@gmail.com>
2014-02-12 01:09:31 -0500

    widen artists table

--- db_models/artists.sql
+++ db_models/artists.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS `artists` (
   `artistId` int(11) NOT NULL AUTO_INCREMENT,
-  `artist` varchar(100) NOT NULL,
+  `artist` varchar(300) NOT NULL,
   PRIMARY KEY (`artistId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
