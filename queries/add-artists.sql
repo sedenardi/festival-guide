9d394b2411cd7054de2b2ed943e7ecdc7767f967
Sanders DeNardi <sedenardi@gmail.com>
2014-04-14 12:17:16 -0400

    add 2 festivals, update 2 existing

--- queries/add-artists.sql
+++ queries/add-artists.sql
@@ -1,4 +1,4 @@
-SET @festivalId = 6; SET @setTime = '2014-04-25';
+SET @festivalId = 20;
 
 drop table if exists a;
 create temporary table a 
@@ -7,7 +7,7 @@ create temporary table a
 );
 
 insert into a(artist)
-VALUES('Santana'),('The Avett Brothers'),('Public Enemy'),('Jason Isbell'),('Rubén Blades & Roberto Delgado Orchestra'),('Laura Mvula'),('Irvin Mayfield & the New Orleans Jazz Orchestra'),('Gregory Porter'),('The Infamous Stringdusters'),('Eric Lindell and the Sunliners, feat. Anson Funderburgh'),('Joe Louis Walker'),('Wayne Toups & ZyDeCajun'),('Afoxé Omô Nilê Ogunjá of Pernambuco-Brazil'),('Steve Riley & the Mamou Playboys'),('Dee-1'),('Nathan & the Zydeco Cha Chas'),('Shamarr Allen & the Underdawgs'),('Honey Island Swamp Band'),('Little Freddie King Blues Band'),('Brass-A-Holics'),('Leah Chase'),('Geno Delafose & French Rockin’ Boogie'),('Sasha Masakowski'),('Lisa Knowles & the Brown Singers'),('BaianaSystem of Bahia-Brazil'),('Chris Thomas King'),('Tim Laughlin'),('Tricia Boutté & International Friends'),('Gal Holiday and the Honky Tonk Revue'),('Lil’ Buck Sinegal Blues Band'),('Shades of Praise'),('Curtis Pierre & Escola de Samba Casa Samba'),('Ginga Mundo Capoeira of Bahia-Brazil with special guests'),('Connie and Dwight Fitch with St. Raymond & St. Leo the Great Gospel Choir'),('Rotary Downs'),('Javier Gutierrez & Vivaz!'),('Bamboula 2000'),('J. Monque’D Blues Band'),('Alexey Marti & Urban Minds'),('Jimmy Breaux & Friends'),('Aurora Nealand & The Royal Roses'),('Brice Miller & Mahogany Brass Band'),('Vox and the Hound'),('Jaryd Lane & The Parish'),('Julio y Cesar Band'),('Jamil Sharif'),('Betty Winn & One A-Chord'),('Stoney Creek Drum and Dance'),('Semolian Warriors Mardi Gras Indians'),('Comanche Hunters Mardi Gras Indians'),('Kirk Joseph’s Backyard Groove'),('Baritone Bliss'),('Maryland Jazz Band of Cologne - Germany'),('Chris Clifton & His All-Stars'),('Sweet Olive String Band'),('The Real Untouchable Brass Band'),('Zulu Social Aid & Pleasure Club'),('Big Nine Social Aid & Pleasure Club'),('David & Roselyn'),('The Showers'),('Tommy Singleton'),('Young Audiences Brass Band Throwdown'),('Black Mohawk Mardi Gras Indians'),('Black Foot Hunters Mardi Gras Indians'),('The RAMS'),('The Sensational Chosen Voices'),('Keep N It Real Social Aid & Pleasure Club'),('We Are One Social Aid & Pleasure Club'),('City of Love Music & Worship Arts Choir'),('Xavier University Jazz Ensemble'),('Wild Apaches Mardi Gras Indians'),('Geronimo Hunters Mardi Gras Indians'),('Lake Forest Charter Jazz Band'),('GrayHawk presents Native Lore and Tales'),('Miss Claudia’s Terrific Traveling Troubadours'),('KCCA Drama Troupe');
+VALUES('New Order'),('Rise Against'),('Kip Moore'),('Ray LaMontagne'),('Joan Jett & The Blackhearts'),('Nas'),('Brand New'),('The Neighbourhood'),('Fitz And The Tantrums'),('Ludacris'),('Five Finger Death Punch'),('The Fray '),('The Head and the Heart'),('Neon Trees'),('The Airborne Toxic Event'),('Tegan and Sara'),('B.o.B.'),('Robert DeLong'),('Melissa Etheridge'),('Bonnie Raitt'),('Atmosphere'),('Girl Talk'),('REO Speedwagon'),('Ben Harper & Charlie Musselwhite'),('ZZ Ward'),('Umphrey\'s McGee'),('Cheap Trick'),('Walk Off The Earth'),('Ziggy Marley'),('Kongos'),('A Great Big World'),('.38 Special'),('Jake Miller'),('Kool and the Gang'),('Switchfoot'),('Jake Bugg'),('Yonder Mountain String Band'),('Bleachers'),('O.A.R.'),('Timeflies'),('Wailers'),('Los Lonely Boys'),('Scotty McCreery'),('Pentatonix'),('SoMo'),('Blackberry Smoke'),('Rusted Root'),('George Thorogood & The Destroyers'),('DJ Pauly D'),('Kansas'),('Nitty Gritty Dirt Band'),('Trombone Shorty & Orleans Avenue'),('Michael McDonald'),('Clay Walker'),('The Crystal Method'),('Dennis DeYoung: The Music Of Styx'),('Matisyahu'),('Crowder'),('Best Coast'),('Taj Mahal Trio'),('David Nail'),('The Hold Steady'),('Delta Rae'),('Cowboy Mouth'),('Saints of Valory'),('Bombino'),('Kopecky Family Band'),('Moon Taxi'),('St. Lucia'),('The Wild Feathers'),('The James Hunter Six'),('San Fermin'),('Bad Suns'),('Kitten'),('Wild Cub'),('Foy Vance'),('Bruno Mars'),('Aloe Blacc'),('Lady Gaga'),('Brad Paisley'),('Darius Rucker'),('Joel Crouse'),('Outkast'),('Gary Clark Jr.'),('Dave Matthews Band'),('Zac Brown Band'),('Motley Crue'),('Alice Cooper'),('Fall Out Boy'),('Paramore'),('New Politics'),('Usher'),('Bebe Rexha');
 
 Insert into artists(artist)
 select artist
@@ -24,4 +24,8 @@ select
 from artists
 where exists
 	(Select 1 from a
-	where a.artist = artists.artist);
+	where a.artist = artists.artist)
+and not exists
+	(Select 1 from appearances app
+	where app.artistId = artists.artistId
+	and app.festivalId = @festivalId);
