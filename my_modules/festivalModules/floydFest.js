936590c8b8a31f8adee4e066d8de539ead430373
Sanders DeNardi <sdenardi@relsci.com>
2015-01-29 18:18:36 -0500

    add FloydFest

---
+++ my_modules/festivalModules/floydFest.js
@@ -0,0 +1,28 @@
+var util = require('util'),
+    Festival = require('./festival.js'),
+    cheerio = require('cheerio');
+
+var FloydFest = function() {
+  FloydFest.super_.call(this);
+};
+
+util.inherits(FloydFest, Festival);
+
+FloydFest.prototype.getFestivalUrls = function() {
+  return [
+    { tag: 'FloydFest', festivalDateId: 9, url: 'http://lineup.floydfest.com/' }
+  ];
+};
+
+FloydFest.prototype.parseFestival = function(fest,data) {
+  var $ = cheerio.load(data);
+
+  fest.artists = $('.ds-level-override').map(function(v,i) {
+    return $(this).find('.w').map(function(w,j){
+      return $(this).find('img').attr('alt').trim();
+    }).get();
+  }).get();
+  this.generateInserts(fest);
+};
+
+module.exports = FloydFest;
