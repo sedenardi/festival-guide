0313e03a1980381f4a67323cd0699817de51ed6b
Sanders DeNardi <sdenardi@relsci.com>
2015-01-29 17:34:03 -0500

    add Bonnaroo

---
+++ my_modules/festivalModules/bonnaroo.js
@@ -0,0 +1,28 @@
+var util = require('util'),
+    Festival = require('./festival.js'),
+    cheerio = require('cheerio');
+
+var Bonnaroo = function() {
+  Bonnaroo.super_.call(this);
+};
+
+util.inherits(Bonnaroo, Festival);
+
+Bonnaroo.prototype.getFestivalUrls = function() {
+  return [
+    { tag: 'Bonnaroo', festivalDateId: 3, url: 'http://lineup.bonnaroo.com/' }
+  ];
+};
+
+Bonnaroo.prototype.parseFestival = function(fest,data) {
+  var $ = cheerio.load(data);
+
+  fest.artists = $('.ds-level-override').map(function(v,i) {
+    return $(this).find('.w').map(function(w,j){
+      return $(this).find('a').text().trim();
+    }).get();
+  }).get();
+  this.generateInserts(fest);
+};
+
+module.exports = Bonnaroo;
