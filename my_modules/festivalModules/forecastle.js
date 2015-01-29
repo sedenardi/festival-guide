7bfd72cf0dc053b85bc9df9b9032ddec3be26d99
Sanders DeNardi <sdenardi@relsci.com>
2015-01-29 17:54:09 -0500

    add Forecastle

---
+++ my_modules/festivalModules/forecastle.js
@@ -0,0 +1,28 @@
+var util = require('util'),
+    Festival = require('./festival.js'),
+    cheerio = require('cheerio');
+
+var Forecastle = function() {
+  Forecastle.super_.call(this);
+};
+
+util.inherits(Forecastle, Festival);
+
+Forecastle.prototype.getFestivalUrls = function() {
+  return [
+    { tag: 'Forecastle', festivalDateId: 6, url: 'http://lineup.forecastlefest.com/' }
+  ];
+};
+
+Forecastle.prototype.parseFestival = function(fest,data) {
+  var $ = cheerio.load(data);
+
+  fest.artists = $('.ds-level-override').map(function(v,i) {
+    return $(this).find('.w').map(function(w,j){
+      return $(this).find('a').text().trim();
+    }).get();
+  }).get();
+  this.generateInserts(fest);
+};
+
+module.exports = Forecastle;
