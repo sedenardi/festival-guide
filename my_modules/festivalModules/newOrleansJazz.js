cb77a8e89b1ab0655e0d843630fe495542c39c14
Sanders DeNardi <sdenardi@relsci.com>
2015-01-29 17:44:23 -0500

    add New Orleans Jazz Fest

---
+++ my_modules/festivalModules/newOrleansJazz.js
@@ -0,0 +1,28 @@
+var util = require('util'),
+    Festival = require('./festival.js'),
+    cheerio = require('cheerio');
+
+var NewOrleansJazz = function() {
+  NewOrleansJazz.super_.call(this);
+};
+
+util.inherits(NewOrleansJazz, Festival);
+
+NewOrleansJazz.prototype.getFestivalUrls = function() {
+  return [
+    { tag: 'New Orleans Jazz', festivalDateId: 4, url: 'http://lineup.nojazzfest.com/' }
+  ];
+};
+
+NewOrleansJazz.prototype.parseFestival = function(fest,data) {
+  var $ = cheerio.load(data);
+
+  fest.artists = $('.ds-level-override').map(function(v,i) {
+    return $(this).find('.w').map(function(w,j){
+      return $(this).find('a').text().trim();
+    }).get();
+  }).get();
+  this.generateInserts(fest);
+};
+
+module.exports = NewOrleansJazz;
