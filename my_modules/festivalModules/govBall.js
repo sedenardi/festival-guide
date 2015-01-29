9b6f5eddc88e5180996c39fc68dbd35f430a3209
Sanders DeNardi <sdenardi@relsci.com>
2015-01-29 17:29:48 -0500

    add Governor's Ball

---
+++ my_modules/festivalModules/govBall.js
@@ -0,0 +1,28 @@
+var util = require('util'),
+    Festival = require('./festival.js'),
+    cheerio = require('cheerio');
+
+var GovBall = function() {
+  GovBall.super_.call(this);
+};
+
+util.inherits(GovBall, Festival);
+
+GovBall.prototype.getFestivalUrls = function() {
+  return [
+    { tag: 'Governor\'s Ball', festivalDateId: 3, url: 'http://lineup.governorsballmusicfestival.com/' }
+  ];
+};
+
+GovBall.prototype.parseFestival = function(fest,data) {
+  var $ = cheerio.load(data);
+
+  fest.artists = $('.ds-level-override').map(function(v,i) {
+    return $(this).find('.w').map(function(w,j){
+      return $(this).find('a').text().trim();
+    }).get();
+  }).get();
+  this.generateInserts(fest);
+};
+
+module.exports = GovBall;
