6f6f3ff72571d73ed5a31f853818be906e2bec91
Sanders DeNardi <sdenardi@relsci.com>
2015-01-29 17:51:26 -0500

    add Wakarusa

---
+++ my_modules/festivalModules/wakarusa.js
@@ -0,0 +1,26 @@
+var util = require('util'),
+    Festival = require('./festival.js'),
+    cheerio = require('cheerio');
+
+var Wakarusa = function() {
+  Wakarusa.super_.call(this);
+};
+
+util.inherits(Wakarusa, Festival);
+
+Wakarusa.prototype.getFestivalUrls = function() {
+  return [
+    { tag: 'Wakarusa', festivalDateId: 5, url: 'http://www.wakarusa.com/artist-lineup/' }
+  ];
+};
+
+Wakarusa.prototype.parseFestival = function(fest,data) {
+  var $ = cheerio.load(data);
+
+  fest.artists = $('#container-lineup').find('li').map(function(v,i) {
+    return $(this).text().trim();
+  }).get();
+  this.generateInserts(fest);
+};
+
+module.exports = Wakarusa;
