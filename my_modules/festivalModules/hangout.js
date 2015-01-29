be1c10be0b54f0d84ec39216072da73260242b81
Sanders DeNardi <sdenardi@relsci.com>
2015-01-29 18:11:03 -0500

    add Hangout

---
+++ my_modules/festivalModules/hangout.js
@@ -0,0 +1,28 @@
+var util = require('util'),
+    Festival = require('./festival.js'),
+    cheerio = require('cheerio');
+
+var Hangout = function() {
+  Hangout.super_.call(this);
+};
+
+util.inherits(Hangout, Festival);
+
+Hangout.prototype.getFestivalUrls = function() {
+  return [
+    { tag: 'Hangout', festivalDateId: 7, url: 'http://lineup.hangoutmusicfest.com/' }
+  ];
+};
+
+Hangout.prototype.parseFestival = function(fest,data) {
+  var $ = cheerio.load(data);
+
+  fest.artists = $('.ds-level-override').map(function(v,i) {
+    return $(this).find('.w').map(function(w,j){
+      return $(this).find('a').text().trim();
+    }).get();
+  }).get();
+  this.generateInserts(fest);
+};
+
+module.exports = Hangout;
